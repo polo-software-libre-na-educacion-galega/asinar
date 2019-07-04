68cc55e8ab1e3389090fdac5419768b4baf14e80
Pablo <pbelay@gmail.com>
2019-07-04 19:09:09 +0200

    Bugs

--- funciones.js
+++ funciones.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 	
 	$("#alertaErros").hide();
-	$(".boton_envio").click(function() {
+	$('#infoasinado').hide();
+	$(".boton_envio").click(function(e) {
+		
+		e.stopPropagation();
+		
 		$("#alertaErros").html("");
 		$("#alertaErros").hide();
 		var tipo =$('input[name=tipo]:checked').val();
@@ -12,22 +16,26 @@ $(document).ready(function(){
 		var concello =  $("#concello").val();  
 		var	validacion_email = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
 	
-		alert(tipo+" "+nome+" "+apelidos+" "+ email +" "+  profesion +" "+ concello+" " );
 		
 		//Validación 
 		var mensaxevalidacion="";
 		if (!(tipo=="0" || tipo=="1")){
 			mensaxevalidacion =mensaxevalidacion+"<li> <b> Error!!</b> Debes indicar se asinas como <b> persoa </b> ou <b> entidade xurídica</b> </li>";
-		} 
+			$("#div_id_asina_como").addClass("has-error");
+		}
+		
 		if (nome === "") {
 			mensaxevalidacion =mensaxevalidacion+"<li> <b> Error!!</b> O campo nome é obrigatorio </li>";
+			$("#divnome2").addClass("has-error");
  		} 
-		alert("merda");
-		if (apelidos === "") {
+		
+		/*if (apelidos === "") {
 			mensaxevalidacion =mensaxevalidacion+"<li> <b> Error!!</b> O campo apelidos é obrigatorio </li>";
-		}
+			$("#div_apelidos").addClass("has-error");
+		}*/
 		if (email === "") {
 			mensaxevalidacion =mensaxevalidacion+"<li> <b> Error!!</b> O campo email é obrigatorio </li>";
+			$("#div_id_email").addClass("has-error");
 		}
 		 
 		if (mensaxevalidacion!=""){
@@ -35,16 +43,26 @@ $(document).ready(function(){
 			$("#alertaErros").show();
 			return false;
 		}
+		
+		//eliminar clase de erros nos inputsç
+		$("#div_id_asina_como").removeClass("has-error");
+		$("#divnome2").removeClass("has-error");
+		$("#div_id_email").removeClass("has-error");
+
+		
  		//
 		$.ajax({
 	    		type: "POST",
 	    		url: "procesarformulario.php",
 				data: "tipo="+tipo+"&nome="+nome+"&apelidos="+apelidos+"&email="+email+"&profesion="+profesion+"&concello="+concello,
 	    		success: function() {
-					alert("Asinado");
+					$('#formularioasinar').hide();
+					$('#infoasinado').show();
+					
 	    		},
 				error: function() {
-					alert("error");				
+					$("#alertaErros").html("Error!! Volve intentar asinar houbo un erro na comunicación co servidor");
+					$("#alertaErros").show();		
 				}
 	   		});
 
--- index.php
+++ index.php
@@ -2,29 +2,40 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
 <!------ Funcions propias  ---------->
 <script src="funciones.js"></script>
 
 </head>
 <body>
 
+
+
 <div class="container">    
             
-    <div id="panelform" style=" margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+    <div  style=" margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
         <div class="panel panel-info">
             <div class="panel-heading">
                 <div class="panel-title">Asinar "Polo software libre na educación galega"</div>
                 <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="https://polo-software-libre-na-educacion-galega.github.io/Manifesto/">Manifesto</a></div>
-            </div>  
+           
+		   </div>  
 			<div id="alertaErros" class="alert alert-danger" style="margin-left: 10px; margin-top: 10px;margin-right: 10px;">
 				<strong>Error!</strong> Indicates a successful or positive action.
 			</div>
 			
-            <div class="panel-body" >
+			
+			<div id="infoasinado" class="alert alert-success" style="margin-left: 10px; margin-top: 10px;margin-right: 10px;">
+				<strong> Moi Ben!!!</strong> Moitas grazas por asinar  "Polo software libre na educacion galega".
+				<br>
+				<a class="twitter-share-button"  href="https://twitter.com/intent/tweet?text=Eu%20asinei%20Polo%20software%20libre%20na%20educación%20galega.%20Podes%20facelo%20aqui%20www.xeopesca.gal/manifesto%20#SoftwareLibre"  data-size="large"> Comparte cos teus amigos esta causa </a>
+			</div>
+			
+            <div id="formularioasinar" class="panel-body" >
                 <form method="post" action=".">
                             
 
-                    <form id="formularioasinar"  class="form-horizontal" method="post" >
+                    <form   class="form-horizontal" method="post" >
                           
                         <div id="div_id_asina_como" class="form-group required">
                             <label for="id_As"  class="control-label col-md-4  requiredField">Asinas como:<span class="asteriskField">*</span> </label>
@@ -33,16 +44,17 @@
                                 <label class="radio-inline"> <input type="radio" name="tipo" id="id_As_2" value="1"  style="margin-bottom: 10px">Entidade xurídica</label>
                             </div>
                         </div>
-                        
-						<div id="divnome" class="form-group required">
-                            <label for="id_nome" class="control-label col-md-4  requiredField">Nome<span class="asteriskField">*</span> </label>
+                      
+						
+						<div id="divnome2" class="form-group ">
+                            <label for="id_nome" class="control-label col-md-4  requiredField">Nome  </label>
                             <div class="controls col-md-8 "> 
-                                <input id="nome" class="input-md textinput textInput form-control"  name="nome" placeholder="O teu nome" style="margin-bottom: 10px" type="text" 
+                                <input class="input-md textinput textInput form-control" id="nome" name="nome" placeholder="Os teus apelidos" style="margin-bottom: 10px" type="text" />
                             </div>
                         </div>
 						
 						
-						<div id="div_apelidos" class="form-group required">
+						<div id="div_apelidos" class="form-group ">
                             <label for="id_apelidos" class="control-label col-md-4  requiredField">Apelidos  </label>
                             <div class="controls col-md-8 "> 
                                 <input class="input-md textinput textInput form-control" id="apelidos" name="apelidos" placeholder="Os teus apelidos" style="margin-bottom: 10px" type="text" />
@@ -65,8 +77,8 @@
                                  <input class="input-md textinput textInput form-control" id="profesion" name="profesion" placeholder="A túa profesión" style="margin-bottom: 10px" type="text" />
                             </div> 
                         </div> 
-                        <div id="div_id_location" class="form-group required">
-                            <label for="id_location" class="control-label col-md-4  requiredField"> Concello<span class="asteriskField">*</span> </label>
+                        <div id="div_id_location" class="form-group">
+                            <label for="id_location" class="control-label col-md-4  requiredField"> Concello </label>
                             <div class="controls col-md-8 ">
                                 <input class="input-md textinput textInput form-control" id="concello" name="concello" placeholder="O teu concello" style="margin-bottom: 10px" type="text" />
                             </div> 
@@ -75,7 +87,7 @@
                         <div class="form-group"> 
                             <div class="aab controls col-md-6 "></div>
                             <div class="controls col-md-6 ">
-                                <input type="submit" name="asinar" value="Asinar" class="btn btn-primary btn btn-primary boton_envio" id="submit-id-signup" />
+                                <input type="" name="asinar" value="Asinar" class="btn btn-primary btn btn-primary boton_envio" id="submit-id-signup" />
                             </div>
                         </div> 
                             
@@ -88,6 +100,12 @@
 	
 	
 </div>
+
+
+<!-- -->
+
+
+
 </div>  
 
 
